4ee5bb8783c4757402c915b89ee2846392db6766
rayhaanalykhan <rayhaanlykhan@gmail.com>
2024-06-26 20:19:04 +0500

    - Search area view complete

--- Spark/Controller/AppFlow/RosterVC.swift
+++ Spark/Controller/AppFlow/RosterVC.swift
@@ -9,10 +9,48 @@ import UIKit
 
 class RosterVC: BaseVC {
     
+    @IBOutlet weak var tableView: UITableView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         
+        initUI()
     }
+    
+    func initUI() {
+
+        // tableView nib registration
+        self.tableView.register(UINib(nibName: "", bundle: nil), forHeaderFooterViewReuseIdentifier: "")
+        
+        self.tableView.delegate = self
+        self.tableView.dataSource = self
+    }
+}
+
+extension RosterVC: UITableViewDataSource, UITableViewDelegate {
+    
+//    func numberOfSections(in tableView: UITableView) -> Int {
+//        return 5
+//    }
+//
+//    func tableView(_ tableView: UITableView, viewForHeaderInSection section: Int) -> UIView? {
+//        
+//        guard let headerView = tableView.dequeueReusableHeaderFooterView(withIdentifier: "SearchHeaderView") as? SearchHeaderView else {
+//            return UIView()
+//        }
+//    
+//        return headerView
+//    }
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        0
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        return UITableViewCell()
+    }
+    
+    
 }
 
 
